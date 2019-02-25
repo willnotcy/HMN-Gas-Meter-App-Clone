4052d59828f71b96c4b6f82f77d40327d8ae8f4f
Benjamin Møller Noer <bemn@itu.dk>
2019-02-25 20:16:59 +0100

    Added rounded corners for android

---
+++ HMNGasApp/HMNGasApp/HMNGasApp.Android/RoundedEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HMNGasApp.Droid;
+using HMNGasApp.View.Components;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(RoundedEntry), typeof(RoundedEntryRenderer))]
+namespace HMNGasApp.Droid
+{
+    public class RoundedEntryRenderer : EntryRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = Xamarin.Forms.Forms.Context.GetDrawable(Resource.Drawable.RoundedEntryText);
+            }
+        }
+    }
+}
