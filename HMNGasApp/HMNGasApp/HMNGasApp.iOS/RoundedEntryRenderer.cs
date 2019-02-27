4254091b0a5098ecbc3bd6c6630fcb029431173b
Benjamin Møller Noer <bemn@itu.dk>
2019-02-27 09:12:05 +0100

    Added customrenderer for iOS, needs testing

---
+++ HMNGasApp/HMNGasApp/HMNGasApp.iOS/RoundedEntryRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using HMNGasApp.iOS;
+using HMNGasApp.View.Components;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(RoundedEntry), typeof(RoundedEntryRenderer))]
+namespace HMNGasApp.iOS
+{
+    public class RoundedEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                // do whatever you want to the UITextField here!
+                Control.BackgroundColor = UIColor.FromRGB(204, 153, 255);
+                Control.BorderStyle = UITextBorderStyle.RoundedRect;
+                
+            }
+        }
+    }
+}
