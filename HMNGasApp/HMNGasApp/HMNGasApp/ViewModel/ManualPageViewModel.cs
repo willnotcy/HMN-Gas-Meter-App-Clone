eae280673d90f2d95d4e44a16595cd97ccb0942b
Tracy Cheung <trac@itu.dk>
2019-02-27 13:04:19 +0100

    Added a ugly back-button

--- HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/ManualPageViewModel.cs
+++ HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/ManualPageViewModel.cs
@@ -4,16 +4,27 @@ using Xamarin.Forms;
 using HMNGasApp.View;
 
 namespace HMNGasApp.ViewModel
-{ 
+{
 
     public class ManualPageViewModel : BaseViewModel
     {
 
         public ICommand ManualCommand { get; set; }
+        public ICommand BackToHomePageCommand { get; set; }
 
         public ManualPageViewModel()
         {
             ManualCommand = new Command(async () => await Navigation.PushAsync(new MainPage()));
+
+            BackToHomePageCommand = new Command(() => BackToHomePage());
+
         }
+
+        async void BackToHomePage()
+        {
+            await Navigation.PopModalAsync();
+
+        }
+      
     }
 }
