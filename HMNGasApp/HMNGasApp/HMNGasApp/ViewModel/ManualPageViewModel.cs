815105b8586d65a496b15a723bacba72cda970db
mkkr <mkkr@itu.dk>
2019-02-27 16:47:18 +0100

    Fixed navigation issues and added implemented logout button. Removed cogwheel from manual page

--- HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/ManualPageViewModel.cs
+++ HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/ManualPageViewModel.cs
@@ -12,21 +12,17 @@ namespace HMNGasApp.ViewModel
 
         public ICommand ManualCommand { get; set; }
         public ICommand ReturnNavCommand { get; set; }
-        public ICommand SettingsPageNavCommand { get; set; }
 
         public ManualPageViewModel()
         {
             ManualCommand = new Command(async () => await ExecuteManualCommand());
             ReturnNavCommand = new Command(async () => await Navigation.PopModalAsync());
-            //SettingsPageNavCommand = new Command(async () => await Navigation.PushModalAsync(new SettingsPage()));
-
         }
 
         private async Task ExecuteManualCommand()
         {
             await App.Current.MainPage.DisplayAlert("Data indsendt", "Din manuelle indtastning er blevet godkendt", "Tilbage til menu");
-            await Navigation.PushModalAsync(new MainPage());
+            await Navigation.PopModalAsync();
         }
-
     }
 }
