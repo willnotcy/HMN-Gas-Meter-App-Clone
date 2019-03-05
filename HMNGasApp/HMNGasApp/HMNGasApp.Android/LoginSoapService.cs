bcde24bf98a0b7b7d16cc4aecc6cbf03e2e6a41a
mkkr <mkkr@itu.dk>
2019-03-05 15:08:10 +0100

    Login implemented with api

---
+++ HMNGasApp/HMNGasApp/HMNGasApp.Android/LoginSoapService.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using HMNGasApp.Droid;
+using HMNGasApp.Droid.HMNGasnet;
+using HMNGasApp.Helpers;
+using HMNGasApp.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LoginSoapService))]
+namespace HMNGasApp.Droid
+{
+    public class LoginSoapService : ILoginSoapService
+    {
+        XellentAPI service;
+
+        public LoginSoapService()
+        {
+            service = new XellentAPI()
+            {
+                Url = "http://xel-webfront-test.gasnet.dk:81/TEST/XellentAPI.asmx"
+            };
+        }
+
+        public async Task<(bool, string)> NewLogin(string customerId, string password)
+        {
+            await Task.Run(() =>
+            {
+                var key = SHA.SHA1Encrypt(string.Format("{0}{1}", customerId, ""));
+
+                var response = service.newLogin(new NewLoginRequest() { NewLogin = new NewLogin { WebLogin = customerId, PassWord = password, EncryptedKey = key } });
+
+                var result = response.ErrorCode.Equals("") ? (true, response.ResponseMessage) : (false, response.ResponseCode);
+
+                return result;
+            });
+            return (false, "Something went wrong");
+        }
+    }
+}
