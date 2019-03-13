a19938dfda75afdefc19822b6c3e8004e3025a8e
mkkr <mkkr@itu.dk>
2019-03-13 11:20:34 +0100

    Added tests for MeterReadingSoapService

---
+++ HMNGasApp/HMNGasApp/HMNGasApp/Services/IMeterReadingSoapService.cs
@@ -0,0 +1,11 @@
+﻿using HMNGasApp.WebServices;
+
+namespace HMNGasApp.Services
+{
+    public interface IMeterReadingSoapService
+    {
+        (bool, Installation) GetInstallations();
+        (bool, MeterReadingOrderResponse) GetMeterReadingOrder(Installation installation);
+        (bool, string) NewMeterReading();
+    }
+}
---
+++ HMNGasApp/HMNGasApp/HMNGasApp/Services/MeterReadingSoapService.cs
@@ -0,0 +1,36 @@
+﻿using HMNGasApp.Model;
+using HMNGasApp.WebServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMNGasApp.Services
+{
+    public class MeterReadingSoapService : IMeterReadingSoapService
+    {
+        private readonly IXellentAPI _client;
+        private readonly IConfig _config;
+
+        public MeterReadingSoapService(IXellentAPI client, IConfig config)
+        {
+            _client = client;
+            _config = config;
+        }
+
+        public (bool, Installation) GetInstallations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (bool, MeterReadingOrderResponse) GetMeterReadingOrder(Installation installation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public (bool, string) NewMeterReading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
