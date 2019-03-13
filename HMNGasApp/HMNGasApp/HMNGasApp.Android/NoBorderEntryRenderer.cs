8044d1bb058715bdfd0fbca2ffc8fb4f7332e29a
SATY <saty@itu.dk>
2019-03-13 14:37:27 +0100

    removed borders from entries

---
+++ HMNGasApp/HMNGasApp/HMNGasApp.Android/NoBorderEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HMNGasApp.Droid;
+using HMNGasApp.View.Components;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(RoundedEntry), typeof(RoundedEntryRenderer))]
+namespace HMNGasApp.Droid
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    public class NoBorderEntryRenderer : EntryRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+#pragma warning restore CS0618 // Type or member is obsolete
+}
---
+++ HMNGasApp/HMNGasApp/HMNGasApp.iOS/NoBorderEntryRenderer.cs
@@ -0,0 +1,20 @@
+﻿using HMNGasApp.iOS;
+using HMNGasApp.View.Components;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NoBorderEntry), typeof(NoBorderEntryRenderer))]
+namespace HMNGasApp.iOS
+{
+    public class NoBorderEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+            
+        }
+    }
+}
