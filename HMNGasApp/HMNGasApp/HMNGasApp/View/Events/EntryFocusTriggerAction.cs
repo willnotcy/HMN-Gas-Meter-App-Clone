00f07e32bf4b16fe75fd1c5987ea4b8114001ec2
mark kristensen <markkristensen@outlook.com>
2019-04-02 12:13:01 +0200

    Added auto focus on entry when clicking pencil

---
+++ HMNGasApp/HMNGasApp/HMNGasApp/View/Events/EntryFocusTriggerAction.cs
@@ -0,0 +1,24 @@
+﻿using HMNGasApp.View.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HMNGasApp.View.Events
+{
+    public class EntryFocusTriggerAction : TriggerAction<NoBorderEntry>
+    {
+        public bool Focused { get; set; }
+
+        protected override async void Invoke(NoBorderEntry sender)
+        {
+            await Task.Delay(1000);
+
+            if (Focused)
+            {
+                sender.Focus();
+            }
+        }
+    }
+}
