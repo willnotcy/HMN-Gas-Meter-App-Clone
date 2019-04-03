c9529bdaec12a763015f9e834f0ef985fa72df68
Ulrik Helmig Bramming Rode <ulro@itu.dk>
2019-04-03 12:44:32 +0200

    Thought I removed this already

--- HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
+++ HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
@@ -69,37 +69,5 @@ namespace HMNGasApp.ViewModel
             return "" + ((Int64) vl);
             
         }
-
-		public void Testsetup()
-		{
-			var entries = new[]{
-				 new Microcharts.Entry(212)
-				 {
-					 Label = "UWP",
-					 ValueLabel = "212",
-					 Color = SKColor.Parse("#2c3e50")
-				 },
-				 new Microcharts.Entry(248)
-				 {
-					 Label = "Android",
-					 ValueLabel = "248",
-					 Color = SKColor.Parse("#77d065")
-				 },
-				 new Microcharts.Entry(128)
-				 {
-					 Label = "iOS",
-					 ValueLabel = "128",
-					 Color = SKColor.Parse("#b455b6")
-				 },
-				 new Microcharts.Entry(514)
-				 {
-					 Label = "Shared",
-					 ValueLabel = "514",
-					 Color = SKColor.Parse("#3498db")
-				 }
-			};
-			GraphData = new LineChart() { Entries = entries };
-
-		}
 	}
 }
