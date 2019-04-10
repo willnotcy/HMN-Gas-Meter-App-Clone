a6618d0eed3d3d82b766506688c9eedbdbab9af0
Benjamin Møller Noer <bemn@itu.dk>
2019-04-10 15:15:08 +0200

    Graph still needs some work, but going the right direction!

--- HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
+++ HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
@@ -31,35 +31,50 @@ namespace HMNGasApp.ViewModel
 		private void Setup()
 		{
 			var readings= _config.MeterReadings;
-            var plotModel = new PlotModel { Title = "Seneste forbrug"};
+            var plotModel = new PlotModel { Title = "Seneste forbrug",
+                                            PlotAreaBorderColor = OxyColors.Transparent
+            };
 
-            var series = new LineSeries();
+            var green = OxyColor.FromRgb(51, 134, 113);
+
+            var series = new LineSeries { StrokeThickness = 5.0,
+                                          MarkerType = MarkerType.Circle,
+                                          MarkerSize = 5,
+                                          //TODO Hardcoded color, since impossible to get the correct from Resource Dictionary ¯\_(ツ)_/¯
+                                          MarkerFill = green,
+                                          Color = green
+                                        };
+            var area = new AreaSeries { Color = series.Color, StrokeThickness = 0};
 
             foreach (var r in readings)
             {
-					var parsedReading = FormatValueLabel(double.Parse(r.Consumption));
-                    var dateTime = Convert.ToDateTime(r.ReadingDate);
-                    series.Points.Add(new DataPoint(DateTimeAxis.ToDouble(dateTime), parsedReading));
+				var parsedReading = double.Parse(r.Consumption);
+                var dateTime = Convert.ToDateTime(r.ReadingDate);
+                var dataPoint = new DataPoint(DateTimeAxis.ToDouble(dateTime), parsedReading);
+                series.Points.Add(dataPoint);
+                area.Points.Add(dataPoint);
             }
 
 			
            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left,
 											   IsPanEnabled = false,
-											   IsZoomEnabled =false
-			});
+											   IsZoomEnabled =false, 
+                                               Title = "Forbrug (m\u00b3)",
+                                               AxislineStyle = LineStyle.Solid,
+                                               AxislineThickness = 5.0
+           });
             plotModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom,
 				                                  StringFormat = "yyyy-MM-dd",
                                                   IsPanEnabled = false,
-                                                  IsZoomEnabled = false
-			});
+                                                  IsZoomEnabled = false,
+                                                  Title = "Dato",
+                                                  AxislineStyle = LineStyle.Solid,
+                                                  AxislineThickness = 5.0
+            });
+            plotModel.Series.Add(area);
             plotModel.Series.Add(series);
 
             GraphData = plotModel;
 		}
-
-        private double FormatValueLabel(double vl)
-        {
-            return ((Int64) vl);
-        }
 	}
 }
