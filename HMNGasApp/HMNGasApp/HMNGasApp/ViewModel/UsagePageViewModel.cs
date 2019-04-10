3a8db7a5069427e43bdd0c4382f4cdfe4ad34c35
Benjamin Møller Noer <bemn@itu.dk>
2019-04-10 12:53:20 +0200

    Changes to graph

--- HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
+++ HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
@@ -21,11 +21,6 @@ namespace HMNGasApp.ViewModel
             set { SetProperty(ref _graphData, value); }
         }
 
-        private double MinimumValue;
-        private double MaximumValue;
-        private DateTime Earliest;
-        private DateTime Latest;
-
         public UsagePageViewModel(IConfig config)
 		{
 			_config = config;
@@ -42,53 +37,29 @@ namespace HMNGasApp.ViewModel
 
             foreach (var r in readings)
             {
-                if (r.ReasonToReading == "Ordinær")
-                {
-					var parsedReading = double.Parse(r.Consumption);
-					CheckValue(parsedReading);
+					var parsedReading = FormatValueLabel(double.Parse(r.Consumption));
                     var dateTime = Convert.ToDateTime(r.ReadingDate);
-                    CheckDate(dateTime);
                     series.Points.Add(new DataPoint(DateTimeAxis.ToDouble(dateTime), parsedReading));
-                }
             }
 
 			
-           plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left/*,
-												Maximum = MaximumValue + (MaximumValue * 0.1),
-												Minimum = MinimumValue - (MinimumValue * 0.1),
-												IsPanEnabled = false,
-												IsZoomEnabled =false,
-				//								MajorTickSize = 7*/
+           plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left,
+											   IsPanEnabled = false,
+											   IsZoomEnabled =false
 			});
             plotModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom,
-				StringFormat = "yyyy-MM-dd"/*,
-												  Minimum = DateTimeAxis.ToDouble(Earliest),
-											  	  Maximum = DateTimeAxis.ToDouble(Latest), 
-												  IsPanEnabled = false,
-												  IsZoomEnabled = false*/
+				                                  StringFormat = "yyyy-MM-dd",
+                                                  IsPanEnabled = false,
+                                                  IsZoomEnabled = false
 			});
             plotModel.Series.Add(series);
 
             GraphData = plotModel;
 		}
 
-        private string FormatValueLabel(double vl)
+        private double FormatValueLabel(double vl)
         {
-            return "" + ((Int64) vl);
-
-        }
-
-        private void CheckValue(double value)
-        {
-            MinimumValue = MinimumValue < value && MinimumValue != default(double) ? MinimumValue : value;
-            MaximumValue = MaximumValue > value ? MaximumValue : value;
-        }
-
-        private void CheckDate(DateTime date)
-        {
-            Earliest = Earliest < date ? Earliest : date;
-            Latest = Latest > date ? Latest : date;
-
+            return ((Int64) vl);
         }
 	}
 }
