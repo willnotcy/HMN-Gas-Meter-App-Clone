7ff611310bc086a755eefc7cc014968030f1cb38
Benjamin Møller Noer <bemn@itu.dk>
2019-04-15 12:29:07 +0200

    Changed to bargraph

--- HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
+++ HMNGasApp/HMNGasApp/HMNGasApp/ViewModel/UsagePageViewModel.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Input;
 using HMNGasApp.Model;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
 using Xamarin.Forms;
+using System.Linq;
 
 namespace HMNGasApp.ViewModel
 {
-    class UsagePageViewModel:BaseViewModel
+    class UsagePageViewModel : BaseViewModel
     {
-		public ICommand ReturnNavCommand { get; set; }
-		private readonly IConfig _config;
+        public ICommand ReturnNavCommand { get; set; }
+        private readonly IConfig _config;
 
         private PlotModel _graphData;
 
@@ -22,59 +24,61 @@ namespace HMNGasApp.ViewModel
         }
 
         public UsagePageViewModel(IConfig config)
-		{
-			_config = config;
-			ReturnNavCommand = new Command(async () => await Navigation.PopModalAsync());
-			Setup();
-		}
+        {
+            _config = config;
+            ReturnNavCommand = new Command(async () => await Navigation.PopModalAsync());
+            Setup();
+        }
 
-		private void Setup()
-		{
-			var readings= _config.MeterReadings;
-            var plotModel = new PlotModel { Title = "Seneste forbrug",
-                                            PlotAreaBorderColor = OxyColors.Transparent
+        private void Setup()
+        {
+            var readings = _config.MeterReadings;
+            var plotModel = new PlotModel
+            {
+                Title = "Seneste forbrug",
+                TitleHorizontalAlignment = TitleHorizontalAlignment.CenteredWithinView,
+                PlotAreaBorderColor = OxyColors.Transparent
             };
 
+            //TODO Hardcoded color, since impossible to get the correct color from Resource Dictionary ¯\_(ツ)_/¯
             var green = OxyColor.FromRgb(51, 134, 113);
 
-            var series = new LineSeries { StrokeThickness = 5.0,
-                                          MarkerType = MarkerType.Circle,
-                                          MarkerSize = 5,
-                                          //TODO Hardcoded color, since impossible to get the correct from Resource Dictionary ¯\_(ツ)_/¯
-                                          MarkerFill = green,
-                                          Color = green
-                                        };
-            var area = new AreaSeries { Color = series.Color, StrokeThickness = 0};
-
-            foreach (var r in readings)
+            var series = new BarSeries
             {
-				var parsedReading = double.Parse(r.Consumption);
-                var dateTime = Convert.ToDateTime(r.ReadingDate);
-                var dataPoint = new DataPoint(DateTimeAxis.ToDouble(dateTime), parsedReading);
-                series.Points.Add(dataPoint);
-                area.Points.Add(dataPoint);
+                StrokeThickness = 1.0
+            };
+
+            readings.Reverse();
+            var dates = new List<string>();
+            for (int i = 0; i < readings.Count; i++)// var r in readings)
+            {
+                var r = readings.ElementAt(i);
+                var parsedReading = double.Parse(r.Consumption);
+                series.Items.Add(new BarItem { Value = parsedReading, Color = green,  });
+                dates.Add(r.ReadingDate);
             }
 
-			
-           plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left,
-											   IsPanEnabled = false,
-											   IsZoomEnabled =false, 
-                                               Title = "Forbrug (m\u00b3)",
-                                               AxislineStyle = LineStyle.Solid,
-                                               AxislineThickness = 5.0
-           });
-            plotModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom,
-				                                  StringFormat = "yyyy-MM-dd",
-                                                  IsPanEnabled = false,
-                                                  IsZoomEnabled = false,
-                                                  Title = "Dato",
-                                                  AxislineStyle = LineStyle.Solid,
-                                                  AxislineThickness = 5.0
+            plotModel.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Top,
+                IsPanEnabled = false,
+                IsZoomEnabled = false,
+                Title = "Forbrug (m\u00b3)",
+                AxislineStyle = LineStyle.Solid
             });
-            plotModel.Series.Add(area);
+            plotModel.Axes.Add(new CategoryAxis
+            {
+                Position = AxisPosition.Left,
+                StringFormat = "yyyy-MM-dd",
+                IsPanEnabled = false,
+                IsZoomEnabled = false,
+                Title = "Dato",
+                AxislineStyle = LineStyle.Solid,
+                ItemsSource = dates
+            });
             plotModel.Series.Add(series);
 
             GraphData = plotModel;
-		}
-	}
+        }
+    }
 }
